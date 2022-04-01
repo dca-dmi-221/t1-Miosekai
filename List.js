2d5a4432c47144dff6e64cb69ecba17d46bd4a0d
Miosekai <1010070539@u.icesi.edu.co>
2022-04-01 18:15:50 -0500

    Nuevas clases para la lista

--- List.js
+++ List.js
@@ -1,11 +1,70 @@
-class List{
+class List {
+    constructor(name, image, songs, app) {
+        this._name = name;
+        this._app = app;
+        this._songs = songs;
+        this._image = image;
+        this._width = 280;
+        this._height = 280;
+        this._songItems = [];
+        this._songs.forEach(song => {
+            this.addSong(song);
+        });
+        this._isSelected = false;
+    }
 
-constructor(){
-    
-}
+    display(i) {
+
+        const offX = 300 * i;
 
 
 
+        this._app.image(this._image, offX, 100, this._width, this._height);
+        this._app.fill(56, 27, 84);
+        this._app.rect(offX,  this._height-40 , this._width, this._height/2);
+        this._app.fill(255);
+        this._app.text(this._name, offX + 20, 270);
 
+        
+        if (this.isMouseOver(i)) {
+            fill(35,35,35,150);
+            this._app.rect(offX,100, this._width, this._height);
+        }
 
+/*         this._songItems.forEach((song, index) => {
+            song.display(index, i);
+        }) */
+    }
+
+    click(i){
+        
+        if(this.isMouseOver(i)){
+            this._isSelected = true;
+        } else {
+            this.isSelected = false;
+        }
+    }
+
+    selectList() {
+        if(this._isSelected){
+            return this._songItems;
+        }
+    }
+
+    addSong(song) {
+        this._songItems.push(new Song(song));
+    }
+
+    isMouseOver(i) {
+        const offX = 300 * i;
+        return (mouseX > 0 + offX && mouseX < this._width + offX && mouseY > 100 && mouseY < this._height+100);
+    }
+
+    get isSelected() {
+        return this._isSelected;
+    }
+
+    set isSelected(val) {
+        this._isSelected = val;
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,252 @@
-const input = document.querySelector('input');
+const SONGS_DATA = [{
 
+  name: "Future Nostalgia",
+  artist: "Dua Lipa",
+  cover: "imagenes/1.png",
+  file: './canciones/1.mp3'
+
+  
+},
+
+{
+  name: "You Right",
+  artist: "Doja Cat y The Weeknd",
+  cover: "imagenes/2.png",
+  file: './canciones/2.mp3'
+},
+
+{
+  name: "Unlock it",
+  artist: "Charlie XCX",
+  cover: "imagenes/3.png",
+  file: './canciones/3.mp3'
+},
+
+{
+  name: "Enemy",
+  artist: "Imagine Dragon",
+  cover: "imagenes/4.png",
+  file: './canciones/4.mp3'
+},
+
+
+{
+
+  name: "Sweetest Pie",
+  artist: "Dua Lipa, Megan Thee Stallion",
+  cover: "imagenes/5.png",
+  file: './canciones/5.mp3'
+},
+
+{
+
+  name: "Jenny - Hearteye Speed Mix",
+  artist: "Studio Killers",
+  cover: "imagenes/6.png",
+  file: './canciones/6.mp3'
+},
+
+{
+
+  name: "Rose",
+  artist: "Nana by Anna Tsuchiya ",
+  cover: "imagenes/7.png",
+  file: './canciones/7.mp3'
+},
+
+{
+
+  name: "Dalla Dalla",
+  artist: "ITZY",
+  cover: "imagenes/8.png",
+  file: './canciones/8.mp3'
+},
+
+{
+
+  name: "I’m Not Okay (I Promise)",
+  artist: "My Chemical Romance",
+  cover: "imagenes/9.png",
+  file: './canciones/9.mp3'
+},
+
+{
+
+  name: "Love Again",
+  artist: "Dua Lipa",
+  cover: "imagenes/10.png",
+  file: './canciones/10.mp3'
+},
+
+{
+
+  name: "Heart Of Glass",
+  artist: "Miley Cyrus",
+  cover: "imagenes/15.png",
+  file: './canciones/15.mp3'
+},
+
+{
+
+  name: "Hot",
+  artist: "Avril Lavigne",
+  cover: "imagenes/16.png",
+  file: './canciones/16.mp3'
+},
+
+{
+
+  name: "Take On Me",
+  artist: "a-ha",
+  cover: "imagenes/17.png",
+  file: './canciones/17.mp3'
+},
+
+];
+
+const LIST_DATA = [{
+  name: "Imagine Dragons PlayList",
+  image: "imagenes/list1.jpg",
+  songs: [{
+    name: "Enemy",
+    artist: "Imagine Dragons",
+    cover: "imagenes/4.png",
+    file: './canciones/4.mp3'
+  }, {
+    name: "Believer",
+    artist: "Imagine Dragons",
+    cover: "imagenes/11.png",
+    file: './canciones/11.mp3'
+  }, {
+    name: "Warriors",
+    artist: "Imagine Dragons",
+    cover: "imagenes/12.png",
+    file: './canciones/12.mp3'
+  },
+  {
+    name: "Whatever It Takes",
+    artist: "Imagine Dragons",
+    cover: "imagenes/13.png",
+    file: './canciones/13.mp3'
+  },
+  {
+    name: "Love",
+    artist: "Imagine Dragons",
+    cover: "imagenes/14.png",
+    file: './canciones/14.mp3'
+  }
+
+  ]
+
+},{
+  name: "Random PlayList",
+  image: "imagenes/list2.jpg",
+  songs: [{
+
+    name: "Future Nostalgia",
+    artist: "Dua Lipa",
+    cover: "imagenes/1.png",
+    file: './canciones/1.mp3'
+  
+  
+  },
+  
+  {
+    name: "You Right",
+    artist: "Doja Cat y The Weeknd",
+    cover: "imagenes/2.png",
+    file: './canciones/2.mp3'
+  },
+  
+  {
+    name: "Unlock it",
+    artist: "Charlie XCX",
+    cover: "imagenes/3.png",
+    file: './canciones/3.mp3'
+  },
+  
+  {
+    name: "Enemy",
+    artist: "Imagine Dragon",
+    cover: "imagenes/4.png",
+    file: './canciones/4.mp3'
+  },
+  
+  
+  {
+  
+    name: "Sweetest Pie",
+    artist: "Dua Lipa, Megan Thee Stallion",
+    cover: "imagenes/5.png",
+    file: './canciones/5.mp3'
+  },
+  
+  {
+  
+    name: "Jenny - Hearteye Speed Mix",
+    artist: "Studio Killers",
+    cover: "imagenes/6.png",
+    file: './canciones/6.mp3'
+  },
+  
+  {
+  
+    name: "Rose",
+    artist: "Nana by Anna Tsuchiya ",
+    cover: "imagenes/7.png",
+    file: './canciones/7.mp3'
+  },
+  
+  {
+  
+    name: "Dalla Dalla",
+    artist: "ITZY",
+    cover: "imagenes/8.png",
+    file: './canciones/8.mp3'
+  },
+  
+  {
+  
+    name: "I’m Not Okay (I Promise)",
+    artist: "My Chemical Romance",
+    cover: "imagenes/9.png",
+    file: './canciones/9.mp3'
+  },
+  
+  {
+  
+    name: "Love Again",
+    artist: "Dua Lipa",
+    cover: "imagenes/10.png",
+    file: './canciones/10.mp3'
+  },
+  
+  {
+  
+    name: "Heart Of Glass",
+    artist: "Miley Cyrus",
+    cover: "imagenes/15.png",
+    file: './canciones/15.mp3'
+  },
+  
+  {
+  
+    name: "Hot",
+    artist: "Avril Lavigne",
+    cover: "imagenes/16.png",
+    file: './canciones/16.mp3'
+  },
+  
+  {
+  
+    name: "Take On Me",
+    artist: "a-ha",
+    cover: "imagenes/17.png",
+    file: './canciones/17.mp3'
+  },]
+}]
+
+/*const input = document.querySelector('input');
 // El monitor de los martes me ayudo con este mecanismo de importar archivos
 console.log(input)
 input.addEventListener('input', e => {
@@ -16,36 +263,42 @@ function createObjectURL(file) {
   } else {
     return null;
   }
-}
+}*/
 
 let pantalla1;
 let vol;
-let cancion1;
+//let cancion1;
 let misSonidos = [];
-let listaImagineDragons = new Lista("PlayList Imagine Dragons");
-
+const listFiles = [];
+const songsFiles = [];
+let app;
+//let listaImagineDragons = new Lista("PlayList Imagine Dragons");
 let currentSoundIndex = 0;
 
 function setup() {
-  createCanvas(1280, 720);
-  imageMode(CENTER);
+
+  createCanvas(1280, 720 + (30 * songsFiles.length));
+  app = new App(this);
+  console.log(listFiles);
+  //imageMode(CENTER);
   vol = createSlider(0, 100, 30, 10);
   vol.position(100, 360);
 
-  listaImagineDragons.agregarCancion("Enemy", "Imagine Dragons", cancion4, sonido4, sonido4.duration());
+ /* listaImagineDragons.agregarCancion("Enemy", "Imagine Dragons", cancion4, sonido4, sonido4.duration());
   listaImagineDragons.agregarCancion("Beliver", "Imagine Dragons", cancion4, sonido11, sonido11.duration());
   listaImagineDragons.agregarCancion("Warriors", "Imagine Dragons", cancion4, sonido12, sonido12.duration());
   listaImagineDragons.agregarCancion("Whatever it Takes", "Imagine Dragons", cancion4, sonido13, sonido13.duration());
   listaImagineDragons.agregarCancion("Love", "Imagine Dragons", cancion4, sonido14, sonido14.duration());
   console.log({
     listaImagineDragons
-  });
+  }); 
+  */
 }
 
 
 function preload() {
   pantalla1 = loadImage("imagenes/pantalla 1.png");
-  cancion1 = loadImage("imagenes/1.png");
+ /*cancion1 = loadImage("imagenes/1.png");
   cancion2 = loadImage("imagenes/2.png");
   cancion3 = loadImage("imagenes/3.png");
   cancion4 = loadImage("imagenes/4.png");
@@ -61,7 +314,7 @@ function preload() {
   cancion14 = loadImage("imagenes/14.png");
   cancion15 = loadImage("imagenes/15.png");
   cancion16 = loadImage("imagenes/16.png");
-  cancion17 = loadImage("imagenes/17.png");
+  cancion17 = loadImage("imagenes/17.png");*/
 
 
   soundFormats('mp3');
@@ -83,9 +336,35 @@ function preload() {
     sonido15 = loadSound('./canciones/15.mp3'),
     sonido16 = loadSound('./canciones/16.mp3'),
     sonido17 = loadSound('./canciones/17.mp3'),
-  ];
-}
+  ]; 
 
+
+  SONGS_DATA.forEach(({ file, cover, name, artist }) => {
+    songsFiles.push({
+      name,
+      artist,
+      song: loadSound(file),
+      cover: loadImage(cover),
+    });
+  });
+
+  LIST_DATA.forEach(({image,name,songs}) => {
+    listFiles.push({
+      name,
+      image: loadImage(image),
+      songs: songs.map(({file, cover, name, artist}) =>{
+        return {
+          name,
+          artist,
+          song: loadSound(file),
+          cover: loadImage(cover),
+        }
+      })
+    })
+  })
+
+}
+/*
 function keyPressed() {
   switch (keyCode) {
     case 39:
@@ -114,9 +393,10 @@ function keyPressed() {
       break;
   }
 }
+*/
 
 /////////
-
+/*
 function cambiar(mode) {
   console.log(currentSoundIndex);
   let jumper = 1;
@@ -138,35 +418,42 @@ function cambiar(mode) {
     currentSoundIndex = 0;
   }
 }
-
+*/
 function draw() {
-  background(220);
+  background(51);
+  
   //image(pantalla1, width / 2, height / 2, width, height);
 
-  rect(800, 400, 60, 60);
-  rect(15, 400, 60, 60);
+  /*rect(800, 400, 60, 60);
+  rect(15, 400, 60, 60); */
 
   misSonidos[currentSoundIndex].setVolume(vol.value() / 100);
   fill(0);
-  listaImagineDragons.canciones.forEach((element, index) => {
+  /*listaImagineDragons.canciones.forEach((element, index) => {
     //console.log(element);
     text(element.nombre, 50, 50 + (index * 120))
     text(element.artista, 50, 70 + (index * 120))
     image(element.imagen, 50, 100 + (index * 120), 50, 50);
     text(element.duracion, 50, 150 + (index * 120));
-  });
+  });*/
   //console.log(vol.value())
 
+  app.draw();
+}
 
+function mouseClicked() {
+  app.click();
+}
+
+function mousePressed() {
+  app.press();
+}
+
+function mouseDragged() {
+  app.drag();
 }
 
 
-/* new Cancion("Future Nostalgia", "Dua Lipa", cancion1, sonido1,),
-            new Cancion("You Right", "Doja cat", cancion2, sonido2,),
-            new Cancion("Unlock it", "Charli XCX", cancion3, sonido3,),
-            new Cancion("Enemy", "Imagine Dragons", cancion4, sonido4,),
-            new Cancion("Enemy", "Imagine Dragons", cancion6, sonido6,),
-            new Cancion("Enemy", "Imagine Dragons", cancion7, sonido7,),
-            new Cancion("Enemy", "Imagine Dragons", cancion8, sonido8,),
-            new Cancion("Enemy", "Imagine Dragons", cancion9, sonido9,),
-            new Cancion("Enemy", "Imagine Dragons", cancion10, sonido10,), */
+function mouseReleased() {
+  app.release();
+}
